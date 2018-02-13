e8296326f7725a5d250a9d939c819fac917e9128
juhnowski <juhnowski@gmail.com>
2018-02-14 01:30:02 +0300

    1.2 The world divides into facts. 1.21 Any one can either be the case or not the case while everething else remains the same.

--- src/main/java/lpht/IsNotTheCase.java
+++ src/main/java/lpht/IsNotTheCase.java
@@ -3,6 +3,8 @@ import java.util.Collection;
 
 /**
  * 1.12 Потому что совокупность всех фактов определяет как все то, что имеет место, так и все то, что не имеет место.
+ * 1.21 Любой факт может иметь место или не иметь место, а все прочее при этом остается тем же самым.
+ * Это изменяет предыдущее высказывание.
  */
-public interface IsNotTheCase extends Collection<Fact> {
+public interface IsNotTheCase extends TheCase {
 }
--- src/main/java/lpht/TheCase.java
+++ src/main/java/lpht/TheCase.java
@@ -1,8 +1,9 @@
 package lpht;
+
+import java.util.Collection;
+
 /**
- * 1.1 Мир есть совокупность фактов, а не предметов.
  * 1.21 Любой факт может иметь место или не иметь место, а все прочее при этом остается тем же самым.
  */
-public class Fact {
-    TheCase theCase;
+public interface TheCase extends Collection<Fact> {
 }
