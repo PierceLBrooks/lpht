64181abfa6c235edc35354a4e0cadc6ff7fd4778
juhnowski <juhnowski@gmail.com>
2018-02-14 00:38:07 +0300

    1.12 For the totality of facts determines what is the case, and also whatever is not the case.

--- src/main/java/IsNotTheCase.java
+++ src/main/java/IsNotTheCase.java
@@ -1,8 +1,7 @@
 import java.util.Collection;
 
 /**
- * 1.
  * 1.12 Потому что совокупность всех фактов определяет как все то, что имеет место, так и все то, что не имеет место.
  */
-public interface IsTheCase extends Collection<Fact> {
+public interface IsNotTheCase extends Collection<Fact> {
 }
