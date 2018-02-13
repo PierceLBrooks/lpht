c0487993b0b91cb1ddafc229cf825e5027daf2e9
juhnowski <juhnowski@gmail.com>
2018-02-14 00:25:30 +0300

    1.1 The World is the totality of facts, not of things. 1.1 The world is determined by the facts, and by their being all the facts.

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,8 +1,10 @@
 import java.util.Collection;
 
 /**
- * 1.
+ * 1. Мир есть все то, что имеет место.
+ * 1.1 Мир есть совокупность фактов, а не предметов.
+ * 1.11 Мир определен фактами и тем, что это все факты.
  */
-public class World {
+public abstract class World implements Collection<Fact>{
     Collection<IsTheCase> all;
 }
