1668a1d59308b7dc4d470c16b94ca9d751344377
juhnowski <juhnowski@gmail.com>
2018-02-14 01:56:21 +0300

    What is the case, the fact, is the existence of atomic facts

--- src/main/java/lpht/AtomicFact.java
+++ src/main/java/lpht/AtomicFact.java
@@ -1,8 +1,11 @@
 package lpht;
 
+import java.util.Collection;
+
 /**
  * 2. То, что имеет место, что является фактом, - это существование атомарных фактов.
  */
 public class AtomicFact extends Fact {
     IsTheCase theCase;
+    Collection<Object> objects;
 }
---
+++ src/main/java/lpht/Object.java
@@ -0,0 +1,7 @@
+package lpht;
+
+/**
+ *
+ */
+public class Object {
+}
